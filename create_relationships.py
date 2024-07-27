1b7ec8372123aa6e5e17fde6806c94fb0791df9c
pankajkesri <156541463+pankajkesri@users.noreply.github.com>
2024-07-26 22:08:27 -0400

    added the all required templates

---
+++ create_relationships.py
@@ -0,0 +1,32 @@
+"""
+Description:
+ Creates the relationships table in the Social Network database
+ and populates it with 100 fake relationships.
+
+Usage:
+ python create_relationships.py
+"""
+import os
+
+# Determine the path of the database
+script_dir = os.path.dirname(os.path.abspath(__file__))
+db_path = os.path.join(script_dir, 'social_network.db')
+
+def main():
+    create_relationships_table()
+    populate_relationships_table()
+
+def create_relationships_table():
+    """Creates the relationships table in the DB"""
+    # TODO: Function body
+    # Hint: See example code in lab instructions entitled "Add the Relationships Table"
+    return
+
+def populate_relationships_table():
+    """Adds 100 random relationships to the DB"""
+    # TODO: Function body
+    # Hint: See example code in lab instructions entitled "Populate the Relationships Table"
+    return 
+
+if __name__ == '__main__':
+   main()
